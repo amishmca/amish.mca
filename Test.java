14574c435cb1160970bb9c1ea25e302de8f89b7b
Amit.Mishra <amish.mca@gmail.com>
2019-06-20 12:28:20 +0530

    Added Element F and print the log

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String[] str = { "A", "B", "C", "D" };
+		String[] str = { "A", "B", "C", "D","F" };
 		for (String s : str) {
-			System.out.println("s: " + s);
+			System.out.println("ssss: " + s);
+			System.out.print(s);
 		}
 	}
 }
