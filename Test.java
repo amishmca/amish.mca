39522fd249bb61e62db56d137ff3bb03298bfeff
Amit.Mishra <Amit.Mishra@LP-AmitMishra.igglobal.com>
2019-06-20 12:26:01 +0530

    Added E element

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String[] str = { "A", "B", "C", "D" };
+		String[] str = { "A", "B", "C", "D", "E" };
 		for (String s : str) {
 			System.out.println("s: " + s);
 		}
